1aa3053dc6b91c5953d60f2219acf5d5e6465f22
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-07-26 20:21:07 -0400

    Autocommit (branches)

--- makes/rubellaSA.mk
+++ makes/rubellaSA.mk
@@ -9,14 +9,11 @@ ms = makestuff
 -include $(ms)/os.mk
 ## -include $(ms)/perl.def
 
-<<<<<<< HEAD
 ## Tested? 2019 Jun 03 (Mon)
-=======
 Ignore += $(ms) Makefile
 Makefile: $(ms)/Makefile
 	touch $@
 
->>>>>>> 5df8884af30d0de552af5b785be4682cbd26f1f7
 $(ms)/Makefile: dir = ~/gitroot/branches/makestuff/
 $(ms)/Makefile: 
 	ls $(dir) Makefile && /bin/ln -s $(dir)
@@ -25,13 +22,10 @@ $(ms)/Makefile:
 
 ## Branch content
 
-<<<<<<< HEAD
 Sources += $(wildcard *.R)
 
 Script.Rout: Script.R
-=======
 code/masterScript.Rout: code/masterScript.R
->>>>>>> 5df8884af30d0de552af5b785be4682cbd26f1f7
 
 ######################################################################
 
@@ -39,9 +33,6 @@ code/masterScript.Rout: code/masterScript.R
 
 -include $(ms)/git.mk
 -include $(ms)/visual.mk
-<<<<<<< HEAD
--include $(ms)/wrapR.mk
-=======
--include $(ms)/stepR.mk
->>>>>>> 5df8884af30d0de552af5b785be4682cbd26f1f7
+## -include $(ms)/wrapR.mk
+## -include $(ms)/stepR.mk
 
